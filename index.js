e8728868bb10e5bc4fe0deffb8506c7a7db1e115
anandanand84 <anand.anand84@gmail.com>
2016-11-11 22:38:11 +0000

    Watch service endpoints

---
+++ index.js
@@ -0,0 +1,35 @@
+var K8s = require('k8s');
+var fs  = require('fs');
+var Redis = require('ioredis');
+const execSync = require('child_process').execSync;
+const spawn = require('child_process').spawn;
+var token = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token').toString();
+var env = process.env;
+var kubeapi = K8s.api({
+    endpoint: 'https://'+env.KUBERNETES_SERVICE_HOST,
+    version: '/api/v1',
+    auth : {
+        token : token
+    },
+    strictSSL : false
+});
+
+
+kubeapi.watch('watch/namespaces/default/endpoints/redis').subscribe(data=> {
+    try {
+        for (let node of data.object.subsets) {
+            let ipAddressInfo = node.addresses;
+            for (let addressInfo of ipAddressInfo) {
+                let ip = addressInfo.ip;
+                let result = execSync('/usr/bin/redis-cli  -h ' + ip + ' cluster meet ' + ipAddressInfo[0].ip + ' 6379')
+                console.log(result.toString());
+            };
+            
+        }
+    }
+    catch (err) {
+        console.error(err);
+    }
+}, err=>{
+    console.error(err); 
+});
